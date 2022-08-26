2f5e0dc9a18214440d147ff0d4f6030c544090a1
DenysBorysiuk <105353877+DenysBorysiuk@users.noreply.github.com>
2022-08-26 06:58:14 +0000

    Deploying to gh-pages from @ vlmakh/goit-team-project-01@ff30b169666d805cc7a1913c6e0bba6a6a64c43b 🚀

---
+++ index.1888555b.js
@@ -0,0 +1,2 @@
+!function(){var e;(e={openModalBtn:document.querySelector("[data-menu-btn]"),mobMenu:document.querySelector("[data-menu]")}).openModalBtn.addEventListener("click",(function(){e.mobMenu.classList.toggle("is-closed")}))}();
+//# sourceMappingURL=index.1888555b.js.map
---
+++ index.b67d77c2.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-menu-btn]"),mobMenu:document.querySelector("[data-menu]")};e.openModalBtn.addEventListener("click",(function(){e.mobMenu.classList.toggle("is-closed")}))})();
+//# sourceMappingURL=index.b67d77c2.js.map
