ecb856d9bf28912b23a0e3ff62f15d7952e48403
vlmakh <vlmakh@gmail.com>
2022-08-28 17:29:02 +0300

    add btnHero

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,10 +1,12 @@
 const refs = {
-  openModalBtn: document.querySelector('[data-modal-open]'),
+  openModalBtnHeader: document.querySelector('[data-modal-open-1]'),
+  openModalBtnHero: document.querySelector('[data-modal-open-2]'),
   closeModalBtn: document.querySelector('[data-modal-close]'),
   modal: document.querySelector('[data-modal]'),
 };
 
-refs.openModalBtn.addEventListener('click', toggleModal);
+refs.openModalBtnHeader.addEventListener('click', toggleModal);
+refs.openModalBtnHero.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
