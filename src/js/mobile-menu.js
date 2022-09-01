bde5361a92e38d1a76977d9aec27e32720d84fd7
vlmakh <vlmakh@gmail.com>
2022-09-01 12:41:05 +0300

    add close of navMenu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,9 +2,11 @@
   const refs = {
     openModalBtn: document.querySelector('[data-menu-btn]'),
     mobMenu: document.querySelector('[data-menu]'),
+    // navHomeRef: document.querySelector('[data-home]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.mobMenu.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.mobMenu.classList.toggle('is-closed');
